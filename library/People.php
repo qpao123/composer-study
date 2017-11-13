3eede8a7f2b12f5bb3e3efb910968a2198565379
zheng.xiang <zhengxiang@whptp.com>
2017-11-13 15:16:02 +0800

    init && dev package

---
+++ library/People.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Core;
+
+class People {
+
+	public function index() {
+		echo 'this is people';
+	}
+}
---
+++ library/Run.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Core;
+
+class Run {
+	public function index() {
+		echo 'this is run';
+	}
+}
